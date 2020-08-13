3d4b742dc05930f08b95feac63d9256ab1b9d0fa
Luis Preciado <luisapreciado99@gmail.com>
2020-08-13 14:13:52 -0500

    Updates

---
+++ precache-manifest.b4dfce48539a02766f91cc45f209c92a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cecc627aa52f467c135142724accfbc",
+    "url": "/finance-visualizer/index.html"
+  },
+  {
+    "revision": "3e6eee861c829c6292be",
+    "url": "/finance-visualizer/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7911cc4e0d5c415902a5",
+    "url": "/finance-visualizer/static/js/2.35aaa13a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/finance-visualizer/static/js/2.35aaa13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6eee861c829c6292be",
+    "url": "/finance-visualizer/static/js/main.96ac38e7.chunk.js"
+  },
+  {
+    "revision": "2a5b660ddd175b96a9c8",
+    "url": "/finance-visualizer/static/js/runtime-main.56664fc0.js"
+  }
+]);
