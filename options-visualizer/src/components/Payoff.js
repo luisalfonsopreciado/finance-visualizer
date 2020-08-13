4a2a25855bf2d09d98eef4be76f07574051c0594
Luis Preciado <luisapreciado99@gmail.com>
2020-08-13 17:40:21 -0500

    payoff updating/symbols.json added

--- options-visualizer/src/components/Payoff.js
+++ options-visualizer/src/components/Payoff.js
@@ -3,37 +3,41 @@ import { useEffect } from "react";
 import * as d3 from "d3";
 import * as nv from "nvd3";
 
-const drawChart = (myData) => {
-  /*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
-  nv.addGraph(function () {
-    var chart = nv.models
-      .lineChart()
-      .margin({ left: 100 }) //Adjust chart margins to give the x-axis some breathing room.
-      .useInteractiveGuideline(true) //We want nice looking tooltips and a guideline!
-      .duration(1500) //how fast do you want the lines to transition?
-      .showLegend(true) //Show the legend, allowing users to turn on/off line series.
-      .showYAxis(true) //Show the y-axis
-      .showXAxis(true); //Show the x-axis
-    chart.xAxis //Chart x-axis settings
-      .axisLabel("Price ($)")
-      .tickFormat(d3.format(",r"));
-
-    chart.yAxis //Chart y-axis settings
-      .axisLabel("Profit ($)")
-      .tickFormat(d3.format(",r"));
-
-    d3.select("#chart svg") //Select the <svg> element you want to render the chart in.
-      .datum(myData) //Populate the <svg> element with chart data...
-      .call(chart); //Finally, render the chart!
-
-    //Update the chart when window resizes.
-    nv.utils.windowResize(function () {
-      chart.update();
-    });
-  });
-};
-
 const Payoff = ({ data, errors }) => {
+  // Clear the Charts
+  d3.selectAll("svg > *").remove();
+
+  const drawChart = (myData) => {
+    /*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
+    nv.addGraph(() => {
+      const newChart = nv.models
+        .lineChart()
+        .margin({ left: 100 }) //Adjust chart margins to give the x-axis some breathing room.
+        .useInteractiveGuideline(true) //We want nice looking tooltips and a guideline!
+        .duration(1500) //how fast do you want the lines to transition?
+        .showLegend(true) //Show the legend, allowing users to turn on/off line series.
+        .showYAxis(true) //Show the y-axis
+        .showXAxis(true); //Show the x-axis
+
+      newChart.xAxis //Chart x-axis settings
+        .axisLabel("Price ($)")
+        .tickFormat(d3.format(",r"));
+
+      newChart.yAxis //Chart y-axis settings
+        .axisLabel("Profit ($)")
+        .tickFormat(d3.format(",r"));
+
+      d3.select("#chart svg") //Select the <svg> element you want to render the chart in.
+        .datum(myData) //Populate the <svg> element with chart data...
+        .call(newChart); //Finally, render the chart!
+
+      // Update the chart when window resizes.
+      nv.utils.windowResize(newChart.update);
+
+      return newChart;
+    });
+  };
+
   useEffect(() => {
     drawChart(data);
   }, [data]);
@@ -59,4 +63,5 @@ const Payoff = ({ data, errors }) => {
     </>
   );
 };
+
 export default Payoff;
